bdd9d5d29509ad2a874dd26b5cdfdc7d0ae5c01c
Mitsunee <michelle@renzio42.com>
2017-05-25 13:48:21 +0200

    filtering test in php

--- frame/twitchframe.php
+++ frame/twitchframe.php
@@ -3,6 +3,7 @@ $err400="<h1>ERROR 400 - BAD REQUEST</h1>";
 if(!isset($_GET['channel'])){http_response_code(400);die($err400);}
 if(!isset($_GET['chat'])){http_response_code(400);die($err400);}
 if(!in_array($_GET['chat'],array("none","left","right"))){http_response_code(400);die($err400);}
+$channel=preg_replace("/[^a-z0-9_]+/i","",$_GET['channel']);
 if($channel==""){http_response_code(400);die($err400);}
 $chatoption=$_GET['chat'];
 ?>
