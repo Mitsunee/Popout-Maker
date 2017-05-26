b305804324d18802d6972e4c901f2279bf834c05
Mitsunee <michelle@renzio42.com>
2017-05-26 13:42:45 +0200

    Removed WatchLater playlist support, Stylized Radio
    as /v/videoID is no longer supported by youtube, the feature got
    removed, therefore simplyfying the form and code a lot. I also added a
    bunch of spacing, while looking through the file.
    
    The new stylized Radio inputs can be found on the twitchpage. They
    require a small amount of css and js in the respective html file (which
    in return adds the ability to change the width) and the wrapper div's id
    has to be added in the 2 marked lines in the css file.
    
    radioValue() now automatically uses names, so the input is now a string
    with the shared name for the radio inputs in question.

--- twitch.php
+++ twitch.php
@@ -7,57 +7,48 @@
 <link rel="shortcut icon" href="/favicon.ico">
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script type="text/javascript">
-function GeneratePopout() { // TODO:
-	/*
-	- if there's chat we need an extra 340px width
-	- REMINDER that resizeTo resizes the full window, so you need to do some calc with outherwidth vs innerwidth
-	- also save the current width and height in a var, so we now which one changed (needs to be biased for height or width because of diagonal resize)
-	*/
+function GeneratePopout() { 
+	//reset error div
+	document.getElementById("channelnameemptyerror").style.display="none";
 	//get inputs
 	input = document.getElementById('popouturl').value.toLowerCase().replace(/[^a-z0-9_]+/gi,"");
-	if(input=="") return false;//nice input
+	if(input=="") {document.getElementById("channelnameemptyerror").style.display="block";return false;};//nice input
 	inputwidth = document.getElementById('inputwidth').value;
 	inputheight = document.getElementById('inputheight').value;
-	chatoption = document.getElementById('showchatchoice');
-	if(chatoption.checked) inputwidth = +inputwidth + 340;//add space for chat, if enabled
-	chatsideoption = $('input[name="chatsidechoice"]:checked').val();
+	chatoption = radioValue("chatsidechoice");
+	if(chatoption!="none") inputwidth = +inputwidth + 340;//add space for chat, if enabled
 	//piece together preferences
 	popoutWindowpreferences = "width=" + inputwidth + ",height=" + inputheight + ",status=no,scrollbars=no,resizable=yes,location=no,menubar=no";
-	/*DEBUG ONLY
-	debug = "";
-	debug += popoutWindowpreferences + "<br>";
-	debug += input + " ";
-	if(chatoption.checked) {debug += "with chat on " + chatsideoption + " side";} else {debug += "without chat";}
-	document.getElementById('debugDIV').innerHTML = debug;
-	*DEBUG END*/
-	windowLink = "/frame/twitchframe.php?channel="+input+"&chat=";
-	if(chatoption.checked) {
-		windowLink = windowLink + chatsideoption;
-	} else {
-		windowLink = windowLink + "none";
-	}
+	windowLink = "/frame/twitchframe.php?channel="+input+"&chat="+chatoption;
 	popoutWindow = window.open(windowLink, "pmpopout", popoutWindowpreferences);
 	popoutWindow.focus();
 }
-function ToggleChat(chatchoicedivvalue) {
-	chatchoicediv = document.getElementById("chatsidediv");
+function ToggleChat(radioNum) {
 	chatsizenote = document.getElementById("chatsizenote");
-	if(chatchoicedivvalue) {
-		chatchoicediv.style.display="block";
-		chatsizenote.style.visibility="visible";
-	} else {
-		chatchoicediv.style.display="none";
+	if(radioValue("chatsidechoice")=="none"){
 		chatsizenote.style.visibility="hidden";
+	} else {
+		chatsizenote.style.visibility="visible";	
 	}
+	document.getElementById("chatsidebg").className=radioNum;
 }
 </script>
 <script src="assets/aspectratio.js"></script>
 <script src="assets/radioValue.js"></script>
 <script src="assets/OpenTextbox.js"></script>
+<style>
+#chatsidediv{width:642px;}
+#chatsidediv .radio-bg{width:212px;}
+.radio-1 .radio-bg {margin-left:0px;}
+.radio-2 .radio-bg {margin-left:214px;}
+.radio-3 .radio-bg {margin-left:428px;}
+input[type='radio']+label.is-radio{width:200px;}
+</style>
 </head>
 <body>
 <h1 id="title">Popout Maker - <span style="color:#6441a5;">Twitch</span></h1><hr>
 <div id="main">
+<div class="errorwrapper" id="channelnameemptyerror" style="display:none;"><img src="../assets/warning.png" alt="!"><div>Channel name cannot be empty</div></div>
 <p>Enter channel name here:<br>
 <input type="text" name="popouturl" value="" id="popouturl" style="width:500px"></p>
 <table>
@@ -74,10 +65,11 @@ function ToggleChat(chatchoicedivvalue) {
 	</tr>
 </table>
 <p>
-	<input type="checkbox" name="showchatchoice" id="showchatchoice" onChange="ToggleChat(this.checked);" checked><label for="showchatchoice"> Show Chat</label>
 	<div id="chatsidediv">
-		<input type="radio" name="chatsidechoice" id="chatsideleft" value="left"><label for="chatsideleft"> Show chat on the left side</label><br>
-		<input type="radio" name="chatsidechoice" id="chatsideright" value="right" checked><label for="chatsideright"> Show chat on the right side</label>
+		<input type="radio" name="chatsidechoice" id="chatsideleft" value="left" onChange="ToggleChat('radio-1');"><label for="chatsideleft" class="is-radio"> Show chat on the left side</label>
+		<input type="radio" name="chatsidechoice" id="chatsidenone" value="none" onChange="ToggleChat('radio-2');"><label for="chatsidenone" class="is-radio"> Don't show chat</label>
+		<input type="radio" name="chatsidechoice" id="chatsideright" value="right" onChange="ToggleChat('radio-3');" checked><label for="chatsideright" class="is-radio"> Show chat on the right side</label>
+		<div style="clear:both;" class="radio-3" id="chatsidebg"><div class="radio-bg"></div></div>
 	</div>
 </p>
 <p><input type="button" onclick="GeneratePopout();" value="Generate Popout"></p>
--- youtube.php
+++ youtube.php
@@ -13,9 +13,6 @@ function GeneratePopout() {
 	//Collect user input
 	input = document.getElementById('popouturl').value;//Input URL
 	doPlaylist = document.getElementById('playlisttrigger').checked;//Check Playlist option
-	if(doPlaylist) {
-		playlistType = radioValue(document.dummyform.includeplaylist);//Check which Playlist option
-	}
 	doAutoplay = document.getElementById('autoplayoption').checked;//Autoplay Option
 	doAnnotations = document.getElementById('annotationsoption').checked;//Annotations Option
 	doLooping = document.getElementById('loopoption').checked;//Looping Option
@@ -23,35 +20,28 @@ function GeneratePopout() {
 	inputHeight = document.getElementById('inputheight').value;//Window height input
 	
 	//
-	popouturl = "http://www.youtube.com/";
-	//select embed type
-	if(doPlaylist) { 
-		if(playlistType == "Watch Later") {
-			popouturl += "v/";//we need Watch Later, so we gotta use the old stuff...
-		} else {
-			popouturl += "embed/";
-		}
-	} else {
-		popouturl += "embed/";
-	}
+	popouturl = "http://www.youtube.com/embed/";
 	//parse input
 	inputVideoID = _GET("v",input);
 	inputPlaylistID = _GET("list",input);
 	//check if there is information missing
-	if(!doPlaylist) if(!Boolean(inputVideoID)) {document.getElementById("videoiderror").style.display="block";return false;}
-	if(doPlaylist) {
-		if(playlistType=="Watch Later") if(!Boolean(inputVideoID)) {document.getElementById("videoiderror").style.display="block";return false;}
-		if(playlistType=="Standard Playlist") if(!Boolean(inputPlaylistID)) {document.getElementById("playlistiderror").style.display="block";return false;}
+	if(!doPlaylist) {
+		if(!Boolean(inputVideoID)) {
+			document.getElementById("videoiderror").style.display="block";
+			return false;
+		}
+	} else {
+		if(!Boolean(inputPlaylistID)) {
+			document.getElementById("playlistiderror").style.display="block";
+			return false;
+		}
 	}
 	//include video-ID if existent
 	if(Boolean(inputVideoID)) popouturl += inputVideoID;
 	//at least try to get fullscreen option :/
 	popouturl += "?fs=1";
 	//include playlist
-	if(doPlaylist) {
-		if(playlistType=="Standard Playlist") popouturl += "&list=" + inputPlaylistID;
-		if(playlistType=="Watch Later") popouturl += "&list=WL";
-	}
+	if(doPlaylist) popouturl += "&list=" + inputPlaylistID;
 	//include other options: Autoplay, Annotations, Looping
 	if(doAnnotations) {popouturl += "&iv_load_policy=1";}else{popouturl += "&iv_load_policy=3";}
 	if(doAutoplay) {popouturl += "&autoplay=1";}else{popouturl += "&autoplay=0";}
@@ -65,7 +55,7 @@ function GeneratePopout() {
 	popoutWindow = window.open(popouturl, "ytpopout", "width=" + inputWidth + ",height=" + inputHeight + ",status=no,scrollbars=yes,resizable=yes,location=no,menubar=no");
 	popoutWindow.focus();
 }
-function _GET(needle,haystack) {//looks for variable needle in haystack
+function _GET(needle,haystack) {//looks for variable needle in haystack (haystack is also a hopefully valid youtube link)
 	needle += "=";
 	LF = haystack.indexOf(needle);
 	if(LF==-1) return 0;
@@ -91,19 +81,28 @@ function _GET(needle,haystack) {//looks for variable needle in haystack
 <div id="main">
 <div class="errorwrapper" id="videoiderror" style="display:none;"><img src="../assets/warning.png" alt="!"><div>Couldn't find video ID.</div></div>
 <div class="errorwrapper" id="playlistiderror" style="display:none;"><img src="../assets/warning.png" alt="!"><div>Couldn't find playlist ID. If there's no playlist in your url, please remove the checkmark below.</div></div>
-<p><label>Enter your youtube url here:<br>
+<p>Enter your youtube url here:<br>
 <input type="url" name="popouturl" value="" id="popouturl"></label></p>
-<label><input type="checkbox" id="playlisttrigger" onChange="OpenTextbox(this,'playlisttriggerbox');"> Include Playlist</label>
-<div id="playlisttriggerbox" style="display:none;"><form name="dummyform">
-<fieldset>
-<label><input type="radio" id="playlist1" name="includeplaylist" value="Standard Playlist" checked="checked"> Standard Playlist (use this if your playlist is included in the url)</label><br>
-<label><input type="radio" id="playlist2" name="includeplaylist" value="Watch Later"> Include Watch Later Playlist </label><span class="note">*<br>The Watch Later Popout window uses the old flash player embed page, as the new one doesn't support the watch later playlist. Fullscreen is sadly not supported all the time.</span>
-</fieldset></form></div>
+<input type="checkbox" id="playlisttrigger"><label for="playlisttrigger">Include Playlist</label>
 <div id="advanced_options_closed" style="display:block;"><input type="button" style="background:url('../assets/expand.gif') 0px 4px no-repeat;padding-left:15px;border:0px;" value="More Options" onClick="OpenTextboxToggle('advanced_options_closed','advanced_options_open');"></div>
-<div id="advanced_options_open" style="display:none;"><input type="button" style="background:url('../assets/collapse.gif') 0px 4px no-repeat;padding-left:15px;border:0px;" value="Less Options" onClick="OpenTextboxToggle('advanced_options_open','advanced_options_closed');"><p><label><input type="checkbox" id="autoplayoption" checked="checked"> Enable Autoplay</label><br>
-<label><input type="checkbox" id="annotationsoption" checked="checked"> Enable Annotations</label><br>
-<label><input type="checkbox" id="loopoption"> Enable Looping </label><span class="note">*<br>Loops single video or the playlist. When looping a single video, youtube creates a temporary playlist with the same video two times.</span></p>
-<table><tr><td>Window width:</td><td><input type="number" name="width" id="inputwidth" min="100" value="1280" onChange="aspectratio(this,'inputheight','keep169option',16);"></td><td rowspan="2"><input type="checkbox" id="keep169option" name="keep169option" checked="checked">Keep 16:9 aspect ratio</tr>
-<tr><td>Window height:</td><td><input type="number" name="height" id="inputheight" min="100" value="720" onChange="aspectratio(this,'inputwidth','keep169option',9);"></td></tr></table></div>
+<div id="advanced_options_open" style="display:none;">
+	<input type="button" style="background:url('../assets/collapse.gif') 0px 4px no-repeat;padding-left:15px;border:0px;" value="Less Options" onClick="OpenTextboxToggle('advanced_options_open','advanced_options_closed');">
+	<p>
+		<label><input type="checkbox" id="autoplayoption" checked="checked"> Enable Autoplay</label><br>
+		<label><input type="checkbox" id="annotationsoption" checked="checked"> Enable Annotations</label><br>
+		<label><input type="checkbox" id="loopoption"> Enable Looping </label><span class="note">*<br>Loops single video or the playlist. When looping a single video, youtube creates a temporary playlist with the same video two times.</span>
+	</p>
+	<table>
+		<tr>
+			<td>Window width:</td>
+			<td><input type="number" name="width" id="inputwidth" min="100" value="1280" onChange="aspectratio(this,'inputheight','keep169option',16);"></td>
+			<td rowspan="2"><input type="checkbox" id="keep169option" name="keep169option" checked="checked">Keep 16:9 aspect ratio</td>
+		</tr>
+		<tr>
+			<td>Window height:</td>
+			<td><input type="number" name="height" id="inputheight" min="100" value="720" onChange="aspectratio(this,'inputwidth','keep169option',9);"></td>
+		</tr>
+	</table>
+</div>
 <p><input type="button" onclick="GeneratePopout();" value="Generate Popout"></p>
 <?php require '/assets/inc/footer.php';?>
