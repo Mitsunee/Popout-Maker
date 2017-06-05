8a0c15d5e17508b13305ea1dfb0238209ed66225
Mitsunee <michelle@renzio42.com>
2017-06-05 07:17:58 +0200

    Presets for twitch
    a bunch of rewrite rules for one pretty simple thing. ```twitch/channelName[/height[/chatside]]```or ```twitch/channelName[/chatside[/height]]```
    the width is calculated on the fly, chatside is limited to `left || nochat || right`
    
    I also added a check in the header file to prevent search engine indexing for links that are using this API.
    
    A smaller thing I did was changing a `."\n"` to `.PHP_EOL`

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -2,7 +2,7 @@
 if($self=="twitch") {
 	echo '<p><a href="http://nowlivebot.com" style="font-size:0.9em;"><img src="/assets/-nl';
 	if(mt_rand(0,100)==42) echo "p";
-	echo '.png" style="position:relative;top:6px;" alt="Now Live"> Now Live <span style="font-size:0.8em">(Stream announcing bot for Discord)</span></a></p>'."\n";
+	echo '.png" style="position:relative;top:6px;" alt="Now Live"> Now Live <span style="font-size:0.8em">(Stream announcing bot for Discord)</span></a></p>'.PHP_EOL;
 }
 ?></main>
 <footer>Made by <a href="http://www.mitsunee.com">Mitsunee</a><span style="float:right;"><a href="https://github.com/Mitsunee/Popout-Maker" class="gitlink">About</a> | Version: 1.1 (<a href="https://github.com/Mitsunee/Popout-Maker/blob/master/CHANGELOG.md">changelog</a>)</span></footer>
--- twitch.php
+++ twitch.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html><?php $self="twitch";?>
+<!DOCTYPE html><?php $self="twitch";
+if(isset($_GET['channel'])) $robotNoIndex = TRUE;?>
 <html>
 <head>
 <title>Popout-Maker Twitch</title>
@@ -46,27 +47,38 @@ input[type='radio']+label.is-radio{width:200px;}
 <main>
 <noscript><div class="errorwrapper" id="noscripterror"><img src="/assets/warning.png" alt="!"><div>Javascript is disabled or not suppported by your browser</div></div></noscript>
 <div class="errorwrapper" id="channelnameemptyerror" style="display:none;"><img src="/assets/warning.png" alt="!"><div>Channel name cannot be empty</div></div>
-<form action="javascript:void(0);" onsubmit="GeneratePopout();return false;">
-	<p>Enter channel name here:<br>
-		<input type="text" name="popouturl" value="" id="popouturl" style="width:500px"></p>
+<form action="javascript:void(0);" onsubmit="GeneratePopout();return false;"><?php
+//Prepare form values
+if(isset($_GET['channel'])) {
+	$PRESET['channel']=preg_replace("/[^a-z0-9_]+/i","",$_GET['channel']);//if a channel was set remove all non-valid characters
+} else {$PRESET['channel']="";}
+if(isset($_GET['h'])) {
+	$PRESET['h']=intval(preg_replace("/[^0-9]+/i","",$_GET['h']));//if a height was set remove all non-number characters and convert to int
+	$PRESET['w']=intval($PRESET['h']/9*16);//pre-calculate the initial width for 16:9 aspectratio
+} else {$PRESET['w']=1280;$PRESET['h']=720;}
+$PRESET['chat']=3;
+if(isset($_GET['chat'])&&$_GET['chat']=="left") $PRESET['chat']=1;
+if(isset($_GET['chat'])&&$_GET['chat']=="nochat") $PRESET['chat']=2;
+?>	<p>Enter channel name here:<br>
+		<input type="text" name="popouturl" value="<?php echo $PRESET['channel'];?>" id="popouturl" style="width:500px"></p>
 	<table>
 		<tr>
 			<td>Window width:</td>
-			<td><input type="number" name="width" id="inputwidth" min="100" value="1280" oninput="aspectratio(this,'inputheight','keep169option',16);"><span id="chatsizenote" class="note">+340</span></td>
+			<td><input type="number" name="width" id="inputwidth" min="100" value="<?php echo $PRESET['w'];?>" oninput="aspectratio(this,'inputheight','keep169option',16);"><span id="chatsizenote" class="note">+340</span></td>
 		</tr>
 		<tr>
 			<td>Window height:</td>
-			<td><input type="number" name="height" id="inputheight" min="100" value="720" oninput="aspectratio(this,'inputwidth','keep169option',9);"></td>
+			<td><input type="number" name="height" id="inputheight" min="100" value="<?php echo $PRESET['h'];?>" oninput="aspectratio(this,'inputwidth','keep169option',9);"></td>
 		</tr>
 		<tr>
 			<td class="note" colspan="2">If activated space for chat will automatically be added<input type="checkbox" id="keep169option" name="keep169option" style="display:none;" checked></td>
 		</tr>
 	</table>
 		<div id="chatsidediv">
-			<input type="radio" name="chatsidechoice" id="chatsideleft" value="left" onChange="ToggleChat('radio-1');"><label for="chatsideleft" class="is-radio"> Show chat on the left side</label>
-			<input type="radio" name="chatsidechoice" id="chatsidenone" value="none" onChange="ToggleChat('radio-2');"><label for="chatsidenone" class="is-radio"> Don't show chat</label>
-			<input type="radio" name="chatsidechoice" id="chatsideright" value="right" onChange="ToggleChat('radio-3');" checked><label for="chatsideright" class="is-radio"> Show chat on the right side</label>
-			<div style="clear:both;" class="radio-3" id="chatsidebg"><div class="radio-bg"></div></div>
+			<input type="radio" name="chatsidechoice" id="chatsideleft" value="left" onChange="ToggleChat('radio-1');"<?php if($PRESET['chat']==1) echo " checked";?>><label for="chatsideleft" class="is-radio"> Show chat on the left side</label>
+			<input type="radio" name="chatsidechoice" id="chatsidenone" value="none" onChange="ToggleChat('radio-2');"<?php if($PRESET['chat']==2) echo " checked";?>><label for="chatsidenone" class="is-radio"> Don't show chat</label>
+			<input type="radio" name="chatsidechoice" id="chatsideright" value="right" onChange="ToggleChat('radio-3');"<?php if($PRESET['chat']==3) echo " checked";?>><label for="chatsideright" class="is-radio"> Show chat on the right side</label>
+			<div style="clear:both;" class="radio-<?php echo $PRESET['chat'];?>" id="chatsidebg"><div class="radio-bg"></div></div>
 		</div>
 	<p><input type="submit" value="Generate Popout"></p>
 </form>
